e9d5a50f79e94f6dd9d58541193b48dfcc448312
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-24 21:55:33 +0300

    Создание локального хранилища

---
+++ app/src/main/java/ru/potemkin/appcrypto2/database/CoinPriceInfoDao.kt
@@ -0,0 +1,20 @@
+package ru.potemkin.appcrypto2.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.potemkin.appcrypto2.pojo.CoinPriceInfo
+
+@Dao
+interface CoinPriceInfoDao {
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate")
+    fun getPriceList():LiveData<List<CoinPriceInfo>>
+
+    @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym")
+    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinPriceInfo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPriceList(priceList:List<CoinPriceInfo>)
+}
