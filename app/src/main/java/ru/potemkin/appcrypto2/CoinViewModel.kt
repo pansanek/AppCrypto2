d8c543892211cdb1fa3d5b3cd7d754aca98d743d
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-25 16:46:34 +0300

    Добавляем и настраиваем ViewModel

---
+++ app/src/main/java/ru/potemkin/appcrypto2/CoinViewModel.kt
@@ -0,0 +1,67 @@
+package ru.potemkin.appcrypto2
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.google.gson.Gson
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import ru.potemkin.appcrypto2.api.ApiFactory
+import ru.potemkin.appcrypto2.database.AppDatabase
+import ru.potemkin.appcrypto2.pojo.CoinPriceInfo
+import ru.potemkin.appcrypto2.pojo.CoinPriceInfoRawData
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDatabase.getInstance(application)
+
+    private val compositeDisposable = CompositeDisposable()
+
+    val priceList = db.coinPriceInfoDao().getPriceList()
+
+    fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+            .map {
+                it.data?.map {
+                    it.coinInfo?.name
+                }?.joinToString { "," }
+            }
+            .flatMap { ApiFactory.apiService.getFullPriceList(fSym = it) }
+            .map { getPriceListFromRawData(it) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                db.coinPriceInfoDao().insertPriceList(it)
+            }, {
+                it.message?.let { it1 -> Log.d("TEST", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getPriceListFromRawData(
+        coinPriceInfoRawData: CoinPriceInfoRawData
+    ):List<CoinPriceInfo>{
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if(jsonObject == null) return result
+        val coinKeySet = jsonObject.keySet()
+        for(coinKey in coinKeySet){
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for(currencyKey in currencyKeySet){
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- app/src/main/java/ru/potemkin/appcrypto2/MainActivity.kt
+++ app/src/main/java/ru/potemkin/appcrypto2/MainActivity.kt
@@ -1,34 +1,19 @@
 package ru.potemkin.appcrypto2
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import io.reactivex.android.schedulers.AndroidSchedulers
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import ru.potemkin.appcrypto2.api.ApiFactory
 
 class MainActivity : AppCompatActivity() {
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
 
-        val disposable = ApiFactory.apiService.getTopCoinsInfo()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST",it.toString())
-            },{
-                it.message?.let { it1 -> Log.d("TEST", it1) }
-            })
-        compositeDisposable.add(disposable)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
-    }
+
 
 }
